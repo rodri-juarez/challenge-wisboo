7e46221049fff64f01e5bbd41ba9eede5198d0ac
rodrigo juarez <juarezr100@gmail.com>
2021-09-23 00:40:09 -0300

    feat(api): add models Form-Question-Option

---
+++ api/src/models/Form.js
@@ -0,0 +1,47 @@
+const { DataTypes } = require('sequelize')
+
+
+module.exports = (sequelize) => {
+    sequelize.define('Form', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+        },
+    });
+
+    sequelize.define('Question', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        question_type: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    });
+
+    sequelize.define('Option', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        option: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    })
+}
