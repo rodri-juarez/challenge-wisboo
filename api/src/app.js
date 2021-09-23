ed998851554aa1243aa0ea05cf6c5040878a5100
rodrigo juarez <juarezr100@gmail.com>
2021-09-23 00:20:29 -0300

    feat(api): add folder and app.js

---
+++ api/src/app.js
@@ -0,0 +1,14 @@
+   
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const server = express()
+
+
+server.use(cors())
+server.use(express.json())
+server.use(morgan('dev'))
+
+server.use('/', router)
+
+module.exports = server;
