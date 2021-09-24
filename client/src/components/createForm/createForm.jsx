6269a81ce492fd7f73c3b795bbd9136bb9185c31
rodrigo juarez <juarezr100@gmail.com>
2021-09-24 15:18:11 -0300

    feat(client): add component createForm

--- client/src/components/createForm/createForm.jsx
+++ client/src/components/createForm/createForm.jsx
@@ -1,27 +1,94 @@
 import style from "./createForm.module.css";
+import Question from "./question";
 
-export default function CreateForm() {
+export default function CreateForm({
+  form,
+  error,
+  handleSelect,
+  handleForm,
+  addQuestion,
+  deleteQuestion,
+  createPoll,
+  setForm
+}) {
   return (
     <section>
       <header>
         <h1>Creación de encuestas</h1>
       </header>
       <form>
-        <div className="title">
+        <div>
           <input
             type="text"
             name="name"
             id="title"
+            value={form.name}
             placeholder="Title"
+            onChange={handleForm}
           />
         </div>
-        <div className="description">
+        <div>
           <input
             type="text"
             name="description"
             id="description"
+            value={form.description}
             placeholder="Description"
+            onChange={handleForm}
           />
+        </div>
+
+        {form.questions.map((element, index) => (
+
+          <div key={index}>
+            {element === null ? null : (
+              <div>
+                <div>
+                  <div>
+                    <select
+                      name="question_type"
+                      id={index}
+                      onChange={handleSelect}
+                    >
+                      <option value="" defaultValue hidden>
+                        Select type of question
+                      </option>
+                      <option value="sm">Selección múltiple</option>
+                      <option value="ss">Selección simpe</option>
+                      <option value="text">Text</option>
+                    </select>
+
+                    <input
+                      type="text"
+                      name="text"
+                      id={index}
+                      value={element.text}
+                      placeholder="Question"
+                      onChange={handleSelect}
+                    />
+                  </div>
+
+                  <Question
+                    type={element.question_type}
+                    index={index}
+                    setForm={setForm}
+                    form={form}
+                  />
+                </div>
+
+                <button name={index} onClick={deleteQuestion}>
+                  x
+                </button>
+              </div>
+            )}
+          </div>
+        ))}
+
+        <button onClick={addQuestion}>Add Question</button>
+
+        
+        <div>
+          <button onClick={createPoll}>Create Poll</button>
         </div>
       </form>
     </section>
--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,14 +1,33 @@
 import CreateForm from "../components/createForm/createForm";
-import PreviewForm from "../components/previewForm/PreviewForm";
+import PreviewForm from "../components/previewForm/previewForm";
 import useForm from "../hooks/useForm";
 import style from "./App.module.css";
 
 function App() {
+  const {
+    form,
+    error,
+    handleSelect,
+    handleForm,
+    addQuestion,
+    deleteQuestion,
+    createPoll,
+    setForm
+  } = useForm();
 
   return (
     <div className={style.divContainer}>
-      <CreateForm />
-      <PreviewForm />
+      <CreateForm
+        form={form}
+        error={error}
+        setForm={setForm}
+        handleSelect={handleSelect}
+        handleForm={handleForm}
+        addQuestion={addQuestion}
+        deleteQuestion={deleteQuestion}
+        createPoll={createPoll}
+      />
+      <PreviewForm form={form} />
     </div>
   );
 }
