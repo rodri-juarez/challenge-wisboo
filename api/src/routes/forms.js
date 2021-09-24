81e45beb591021912c74e263d4b12514b4fa43df
rodrigo juarez <juarezr100@gmail.com>
2021-09-23 21:38:04 -0300

    feat(api):route GET by id

--- api/src/routes/forms.js
+++ api/src/routes/forms.js
@@ -1,9 +1,42 @@
-const router = require('express').Router()
-const { Form, Question, Option } = require('../db');
+const router = require("express").Router();
+const { Form } = require("../db");
 
+router.get("/forms/:id", async (req, res) => {
+  const id = req.params.id;
 
-router.get('/forms/:id', async (req, res) => {
+  const form = await Form.findByPk(id, {
+    include: { all: true, nested: true },
+  });
 
-})
+  if (form === null) {
+    return res.status(400).json({ message: "Form does not exist" });
+  }
 
-module.exports = router;  
+  const response = {
+    id: form.dataValues.id,
+    name: form.dataValues.name,
+    description: form.dataValues.description,
+    questions: [],
+  };
+
+  form.Questions.map((element, index) => {
+    response.questions.push({
+      id: element.dataValues.id,
+      question_type: element.dataValues.question_type,
+      text: element.dataValues.text,
+      options: [],
+    });
+
+    if (element.dataValues.question_type.toLowerCase() !== "text") {
+      element.Options.map((element, indexOption) => {
+        response.questions[index].options.unshift(
+          form.Questions[index].Options[indexOption].dataValues.option
+        );
+      });
+    }
+  });
+
+  return res.json(response);
+});
+
+module.exports = router;
