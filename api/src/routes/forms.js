4f4eb22be57eef9446fbc6e6fa612a2d1a82786f
rodrigo juarez <juarezr100@gmail.com>
2021-09-23 22:07:43 -0300

    feat(api): route POST

--- api/src/routes/forms.js
+++ api/src/routes/forms.js
@@ -39,4 +39,53 @@ router.get("/forms/:id", async (req, res) => {
   return res.json(response);
 });
 
+router.post("/forms", async (req, res) => {
+  const { form } = req.body;
+
+  if (!form) {
+    return res.status(400).json({ error: "Form not found in request" });
+  }
+  if (!form.name || !form.description || form.questions.length < 1) {
+    return res
+      .status(400)
+      .json({ error: "Fields or questions are missing from the form" });
+  }
+
+  try {
+    let newForm = await Form.create({
+      name: form.name,
+      description: form.description,
+    });
+
+    while (form.questions.length) {
+      let question = form.questions.shift();
+
+      let newQuestion = await Question.create({
+        question_type: question.question_type,
+        text: question.text,
+      });
+
+      await newForm.addQuestions(newQuestion);
+
+      while (question.options.length) {
+        let option = question.options.pop();
+
+        let newOption = await Option.create({
+          option: option,
+        });
+
+        await newQuestion.addOption(newOption);
+      }
+    }
+
+    let Form = await Form.findAll();
+    return res.json({
+      message: "Form created",
+      formId: Form[Form.length - 1].dataValues.id,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Can't saving form in DB", error: error });
+  }
+});
+
 module.exports = router;
